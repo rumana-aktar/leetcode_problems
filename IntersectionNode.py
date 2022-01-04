b3446dc99b639570e8671a0a56583422c20a21f4
Rumana Aktar <rayy7@mail.missouri.edu>
2022-01-03 22:21:02 -0500

    0160. Intersection of Two Linked Lists: LinkedList

--- IntersectionNode.py
+++ IntersectionNode.py
@@ -11,7 +11,7 @@
 # # ---------------------------------------------------------------------------
 
 # # ---------------------------------------------------------------------------
-## leetcode problem: 160. Intersection of Two Linked Lists
+## leetcode problem: 0160. Intersection of Two Linked Lists
 
 # Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
 # For example, the following two linked lists begin to intersect at node c1:
