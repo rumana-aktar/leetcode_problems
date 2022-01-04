54671a3ce30a3d0986110c03065dfd395cdc9a5b
Rumana Aktar <rayy7@mail.missouri.edu>
2022-01-03 22:18:51 -0500

    0020. Valid Parentheses: Stack

--- BracketMatching.py
+++ BracketMatching.py
@@ -11,7 +11,7 @@
 # # ---------------------------------------------------------------------------
 
 # # ---------------------------------------------------------------------------
-## leetcode problem 
+## leetcode problem: 0020. Valid Parentheses
 # Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 
 # An input string is valid if:
