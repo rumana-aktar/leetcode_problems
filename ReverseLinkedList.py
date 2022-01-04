3f682c95532e7cc650685358fe0ff5ec8cfd8536
Rumana Aktar <rayy7@mail.missouri.edu>
2022-01-03 22:27:08 -0500

    0206. Reverse Linked List: LinkedList

--- ReverseLinkedList.py
+++ ReverseLinkedList.py
@@ -11,7 +11,7 @@
 # # ---------------------------------------------------------------------------
 
 # # ---------------------------------------------------------------------------
-## leetcode problem: 206. Reverse Linked List
+## leetcode problem: 0206. Reverse Linked List
 
 # Given the head of a singly linked list, reverse the list, and return the reversed list.
 
