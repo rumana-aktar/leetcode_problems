6aefbb26d6ef84df5b72e4d685104e67ff40f8b8
Rumana Aktar <rayy7@mail.missouri.edu>
2022-01-03 22:22:38 -0500

    0141. Linked List Cycle: LinkedList

--- LinkedListCycle.py
+++ LinkedListCycle.py
@@ -11,7 +11,7 @@
 # # ---------------------------------------------------------------------------
 
 # # ---------------------------------------------------------------------------
-## leetcode problem: 141. Linked List Cycle 
+## leetcode problem: 0141. Linked List Cycle 
 
 # Given head, the head of a linked list, determine if the linked list has a cycle in it.
 # There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. 
